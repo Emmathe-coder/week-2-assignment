26b076af869311facb022c008bef62d4372daddc
Emmanuel Coffie <coffemmanuel@gmail.com>
2025-09-14 15:02:25 +0000

    my assignment

---
+++ main.py
@@ -0,0 +1,21 @@
+# creating an empty list and appending the figures
+my_list = [10, 20, 30, 40]
+
+# inserting the value 15 at the second position in the list
+my_list.insert(1, 15)
+my_list
+
+# extending list with [50, 60, 70]
+my_list.extend([50, 60, 70])
+my_list
+
+# removing the last element from my_list
+my_list.pop()
+my_list
+
+# sorting my_list in ascending order
+sorted(my_list)
+
+# printing the index of the value 30 
+my_list.index(30)
+
